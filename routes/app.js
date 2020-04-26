3e73a0b074383ddeae31045e82fca5f693488b92
HRY <toto850105@gmail.com>
2020-04-27 00:24:28 +0800

    can get  arduino data

--- routes/app.js
+++ routes/app.js
@@ -1,16 +1,39 @@
 const express = require("express")
 const fs = require("fs")
 const path = require("path")
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
 
+let arduinoPort = '/dev/tty.usbmodem14201'  // for MacOS
+// let arduinoPort = 'COM3' // for Windows
+// let arduinoPort = '/dev/tty.ACM0'    // for Linux
+const port = new SerialPort(
+    arduinoPort , {
+        baudRate: 115200
+    }
+)
+
+global.sernsorData = {}
+
+const parser = port.pipe(new Readline({ delimiter: '\n' }))
+parser.on('data', (data) => {
+    global.sensorData = JSON.parse(data)
+})
+
+const webPort = 8888
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, "../dist")))
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     let html = fs.readFileSync(path.resolve(__dirname, "../dist/index.html"))
     res.send(html)
 })
 
-app.listen(8888, () => {
-    console.log("start server")
+app.post("/data", (req, res) => {
+    res.send(global.sensorData)
+})
+
+app.listen(webPort, () => {
+    console.log(`start server on ${webPort}`)
 })
